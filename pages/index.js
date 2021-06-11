4aa0f0af46c4549f4bacd41492e6a2045e8c85bb
willsgq <76426765+willsgq@users.noreply.github.com>
2021-06-11 14:40:36 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+
+
+
+
+const Home = () => (
+   <div>
+     Olá 
+     <Button color="danger">Danger!</Button>
+   </div>
+
+);
+
+export default Home;
